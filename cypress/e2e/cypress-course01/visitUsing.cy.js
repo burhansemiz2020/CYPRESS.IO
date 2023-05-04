57f13febaf1a5e0a6f738ab669a047e1aa7ab150
https://github.com/burhansemiz2020 <semizburhan017@gmail.com>
2023-05-04 21:06:25 +0200

    04-05-2023

--- cypress/e2e/cypress-course01/visitUsing.cy.js
+++ cypress/e2e/cypress-course01/visitUsing.cy.js
@@ -1,11 +1,11 @@
 describe('Basic commands', () => {
-    it('Basic commands cy.visit using', () => {
+    it.only('Basic commands cy.visit using', () => {
         cy.visit("/")//base url configde tanimlandiysa
         cy.visit("/commands")//base url tanimlandiysa configde tanimlandiysa. 
         //eger command a gidilecekse slashtan sonrasina command yazilir
         cy.visit("https://example.cypress.io/comma")//baseUrl tanimlanmadiysa yani config te yoksa
         cy.visit({
-            url: "https://example.cypress.io", //baseUrl tanimlanmadiysa
+            url: "https://example.cypress.io/", //baseUrl tanimlanmadiysa
             method:'GET'
         })
     });
@@ -16,8 +16,33 @@ describe('Basic commands', () => {
         cy.title().should('include',"cypress")//title icinde cypress geciyorsa dogrular
         cy.title().should('contain',"cypress")//title icinde cypress geciyorsa dogrular
     });
-
+    //url  dogrulama
     it('Temel komutlar cy.url ve cy.location Kullanimi',()=>{
+        cy.url().should('eq','http://example.cypress.io/commands')//url tam esitlik olursa dogrular
+        cy.url().should('include','/commands')//url icerisinde commands gecmesi dogrulama icin yetrlidir
+
+        cy.location('pathname').should('eq','/commands')//gidilen path in dogrulamasi yapiliyor
+        cy.location('protocol').should('eq',"https")//gidilen protokolu dogrular
+        cy.location('hostname').should('eq',"https://example.cypress.io/")//tam esitlikte dogrular
+        cy.location('hostname').should('include',"cypress.io")//cypress.io icerisinde hostname dogrular
     
     }); 
+
+    it('Temel komutlar cy.get kullanimi',()=>{
+        cy.get('#button')//bu kullanim ile should veya click ile buttonu clikleyebiliriz
+        cy.get('#button').as('deneme')//bu kullanim dan sonra cy.get('@deneme')
+        cy.get('a[href="example.cypress.io"]')
+        cy.get('.dropdown-menu-list')
+        cy.get('div.button') //
+        cy.get('ul li:first')
+    });
+
+    it('Temel komutlar cy.log kullanimi', () => {
+        cy.log('Temel komutlar cy.log kullanimi')
+        cy.log('be.visible')
+        cy.log('Bir mesaj',['bir mesaj', 'iki mesaj', 'uc mesaj'])//output: Bir mesaj, [bir mesaj, iki mesaj, uc mesaj] 
+    });
+
+    
+    
 });
